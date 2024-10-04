6ee0338246c7fd054922a108099b8c20be2ec118
Alex Shan <shan-mx@hotmail.com>
2024-10-04 09:42:40 +0800

    chore: sort js rules order (#3)

--- src/configs/js.ts
+++ src/configs/js.ts
@@ -8,18 +8,19 @@ import type { TypedFlatConfigItem } from "../types";
 
 const config: TypedFlatConfigItem = {
   plugins: {
-    "@stylistic": stylistic,
-    "unused-imports": unusedImports,
     unicorn,
     perfectionist,
+    "unused-imports": unusedImports,
+    "@stylistic": stylistic,
   },
   rules: {
     ...js.configs.recommended.rules,
     ...unicorn.configs["flat/recommended"].rules,
     eqeqeq: "error",
     "max-params": ["error", 3],
-    "no-multiple-empty-lines": ["warn", { max: 1, maxEOF: 1 }],
     "no-nested-ternary": "error",
+    "no-lonely-if": "warn",
+    "no-multiple-empty-lines": ["warn", { max: 1, maxEOF: 1 }],
     "prefer-arrow-callback": "warn",
     "no-unneeded-ternary": "warn",
     "one-var-declaration-per-line": ["warn", "always"],
@@ -32,6 +33,19 @@ const config: TypedFlatConfigItem = {
       },
       {
         enforceForRenamedProperties: false,
+      },
+    ],
+    "unicorn/prevent-abbreviations": "off",
+    "unicorn/no-null": "off",
+    "unicorn/prefer-code-point": "off",
+    "unicorn/no-process-exit": "off",
+    "unicorn/prefer-ternary": "off",
+    "unicorn/no-lonely-if": "warn",
+    "unicorn/better-regex": "warn",
+    "unicorn/consistent-function-scoping": [
+      "warn",
+      {
+        checkArrowFunctions: false,
       },
     ],
     "perfectionist/sort-imports": [
@@ -52,6 +66,7 @@ const config: TypedFlatConfigItem = {
     "perfectionist/sort-named-imports": "warn",
     "perfectionist/sort-array-includes": "warn",
     "perfectionist/sort-enums": "warn",
+    "unused-imports/no-unused-imports": "warn",
     "@stylistic/padding-line-between-statements": [
       "warn",
       {
@@ -62,18 +77,6 @@ const config: TypedFlatConfigItem = {
       { blankLine: "always", prev: ["interface", "type"], next: "*" },
       { blankLine: "always", prev: "*", next: ["export", "return"] },
     ],
-    "unused-imports/no-unused-imports": "warn",
-    "unicorn/prevent-abbreviations": "off",
-    "unicorn/no-null": "off",
-    "unicorn/consistent-function-scoping": [
-      "warn",
-      {
-        checkArrowFunctions: false,
-      },
-    ],
-    "unicorn/prefer-code-point": "off",
-    "unicorn/no-process-exit": "off",
-    "unicorn/prefer-ternary": "off",
   },
 };
 
