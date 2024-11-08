dec15c32e2300f7bb597185be219f8c61d22c945
Alex Shan <shan-mx@hotmail.com>
2024-11-08 10:31:32 +0800

    chore(ts): update config

--- src/configs/ts.ts
+++ src/configs/ts.ts
@@ -23,16 +23,13 @@ const config: TypedFlatConfigItem = {
   rules: {
     ...tsPlugin.configs["stylistic-type-checked"].rules,
     ...tsPlugin.configs["recommended-type-checked"].rules,
-    "@typescript-eslint/no-unused-vars": [
-      "warn",
-      { argsIgnorePattern: "^_", varsIgnorePattern: "^_" },
-    ],
+    "@typescript-eslint/no-namespace": "off",
     "@typescript-eslint/consistent-type-imports": [
       "error",
       { prefer: "type-imports", fixStyle: "separate-type-imports" },
     ],
     "@typescript-eslint/no-misused-promises": [
-      2,
+      "error",
       { checksVoidReturn: { attributes: false } },
     ],
     "@typescript-eslint/prefer-for-of": "error",
@@ -49,6 +46,11 @@ const config: TypedFlatConfigItem = {
         format: ["PascalCase"],
       },
     ],
+    "@typescript-eslint/no-unused-vars": [
+      "warn",
+      { argsIgnorePattern: "^_", varsIgnorePattern: "^_" },
+    ],
+    "@typescript-eslint/return-await": "warn",
   },
 };
 
